f519e0456fb56af29aae23a863bea0b8e0bc9b6e
pjay79 <praveen.jayarajan@gmail.com>
2019-03-27 23:46:20 +1100

    Setup expo bare workflow with linear gradient unimodule

---
+++ App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const App = () => (
+  <View style={styles.container}>
+    <LinearGradient
+      colors={['#48c6ef', '#6f86d6']}
+      style={{
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Text style={styles.header}>Expo Starter</Text>
+      <Text style={styles.subHeader}>UNIMODULES</Text>
+      <Text style={styles.subHeader}>[ Linear Gradient ]</Text>
+    </LinearGradient>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    marginBottom: 10,
+  },
+  subHeader: {
+    fontSize: 14,
+    letterSpacing: 4,
+    color: '#FFFFFF',
+  },
+});
+
+export default App;
---
+++ ios/Pods/Target Support Files/Pods-expostarter/Pods-expostarter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_expostarter : NSObject
+@end
+@implementation PodsDummy_Pods_expostarter
+@end
